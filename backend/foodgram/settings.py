db2ce00444005c95f75cf9becb65f8e026cf5f44
Vitaly Fedosov <vkfedosov@yandex.ru>
2023-02-01 20:23:58 +0300

    add logger

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -56,6 +56,33 @@ TEMPLATES = [
     },
 ]
 
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'default': {
+            'format': '[DJANGO] %(levelname)s %(asctime)s %(module)s '
+                      '%(name)s.%(funcName)s:%(lineno)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'default',
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        }
+    },
+}
+
+
 WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
